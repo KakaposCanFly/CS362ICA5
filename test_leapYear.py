cc30837c93f95603b944bbd34db21817d89bc0b5
Cole Theodore <theodoco@oregonstate.edu>
2021-05-20 16:22:41 -0700

    added testing module for unittest

---
+++ test_leapYear.py
@@ -0,0 +1,15 @@
+import unittest
+
+class Testing(unittest.TestCase):
+    def test_leapYear(self):        #will pass if input is leap year
+        result = leapYear.is_leap_year(2002)
+        self.assertTrue(result)
+
+    def test_leapYear2(self):       #will pass if input is NOT leap year
+        result = leapYear.is_leap_year(2002)
+        self.assertFalse(result)
+
+        
+
+if __name__ == "__main__":
+    unittest.main()
